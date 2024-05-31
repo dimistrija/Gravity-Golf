9501342801513648e82a8012d6d277d3f8e63435
dimistrija <147159188+dimistrija@users.noreply.github.com>
2024-05-31 20:32:34 +0200

    first draft

--- test file.py
+++ test file.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import math
+import random
 
 # Initialize Pygame
 pygame.init()
@@ -10,6 +11,8 @@ SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
 BALL_H = 66
 BALL_W = 30
 TARGET_ZONE_SIZE = 60
+OBJECT_SIZE = 40
+
 LEVELS = [
     {'background': 'assets/bck1.png', 'target': (60, SCREEN_HEIGHT - 60)},
     {'background': 'assets/bck2.png', 'target': (SCREEN_WIDTH - 60, SCREEN_HEIGHT - 60)},
@@ -17,6 +20,12 @@ LEVELS = [
 ]
 END_SCREEN_IMG = 'assets/end.png'
 POINTER_IMG = 'assets/pointer.png'
+OBJECT_IMGS = ['assets/cat1.png', 'assets/cat2.png', 'assets/cat3.png']
+SAVED_IMG = 'assets/saved.png'
+saved_img = pygame.image.load(SAVED_IMG)
+saved_img = pygame.transform.scale(saved_img, (333, 53))
+
+
 
 
 # Initialize screen
@@ -28,6 +37,8 @@ ball_img = pygame.image.load('assets/ball.png')
 ball_img = pygame.transform.scale(ball_img, (BALL_H, BALL_W))
 pointer_img = pygame.image.load(POINTER_IMG)
 pointer_img = pygame.transform.scale(pointer_img, (120, 5))
+object_imgs = [pygame.image.load(img) for img in OBJECT_IMGS]
+object_imgs = [pygame.transform.scale(img, (OBJECT_SIZE, OBJECT_SIZE)) for img in object_imgs]
 
 # Game variables
 current_level = 0
@@ -36,6 +47,11 @@ ball_velocity = [0, 0]
 shooting = False
 click_start_time = None
 hold_time = 0
+custom_objects = []
+display_collected = False
+display_start_time = 0
+
+
 
 def load_background(level):
     bg = pygame.image.load(LEVELS[level]['background'])
@@ -47,6 +63,10 @@ def rotate_image(image, angle):
 def check_collision(pos, target):
     return (target[0] - TARGET_ZONE_SIZE / 2 <= pos[0] <= target[0] + TARGET_ZONE_SIZE / 2 and
             target[1] - TARGET_ZONE_SIZE / 2 <= pos[1] <= target[1] + TARGET_ZONE_SIZE / 2)
+
+def check_object_collision(ball_pos, obj_pos):
+    return (obj_pos[0] - OBJECT_SIZE / 2 <= ball_pos[0] <= obj_pos[0] + OBJECT_SIZE / 2 and
+            obj_pos[1] - OBJECT_SIZE / 2 <= ball_pos[1] <= obj_pos[1] + OBJECT_SIZE / 2)
 
 def game_over():
     end_screen = pygame.image.load(END_SCREEN_IMG)
@@ -63,10 +83,18 @@ def bounce_ball():
     if ball_position[1] <= 0 or ball_position[1] >= SCREEN_HEIGHT:
         ball_velocity[1] = -ball_velocity[1]
 
+def spawn_custom_objects(num_objects):
+    for _ in range(num_objects):
+        x = random.randint(OBJECT_SIZE, SCREEN_WIDTH - OBJECT_SIZE)
+        y = random.randint(OBJECT_SIZE, SCREEN_HEIGHT - OBJECT_SIZE)
+        img = random.choice(object_imgs)
+        custom_objects.append({'pos': [x, y], 'img': img})
+
 # Main game loop
 running = True
+spawn_custom_objects(3)  # Initial spawn of 3 custom objects
+
 while running:
-    #screen.fill(WHITE)
     background = load_background(current_level)
     screen.blit(background, (0, 0))
 
@@ -111,6 +139,33 @@ while running:
     else:
         screen.blit(ball_img, (ball_position[0] - BALL_H // 2, ball_position[1] - BALL_W // 2))
 
+
+
+
+
+
+    # Draw the custom objects
+    for obj in custom_objects:
+        screen.blit(obj['img'], obj['pos'])
+
+    current_time = pygame.time.get_ticks()
+    if display_collected and current_time - display_start_time < 3000:  # Display for 3 seconds
+        screen.blit(saved_img, (
+        SCREEN_WIDTH // 2 - saved_img.get_width() // 2, SCREEN_HEIGHT // 2 - saved_img.get_height() // 2))
+    else:
+        display_collected = False
+
+    for obj in custom_objects[:]:
+        if check_object_collision(ball_position, obj['pos']):
+            custom_objects.remove(obj)
+
+            # Set the flag and start time to display the collected image
+            display_collected = True
+            display_start_time = pygame.time.get_ticks()
+
+            if not custom_objects:  # If all objects are collected, spawn new ones
+                spawn_custom_objects(3)
+
     # Draw the pointer
     mouse_pos = pygame.mouse.get_pos()
     pointer_angle = math.degrees(math.atan2(mouse_pos[1] - ball_position[1], mouse_pos[0] - ball_position[0]))
@@ -123,4 +178,3 @@ while running:
 
 pygame.quit()
 sys.exit()
-
